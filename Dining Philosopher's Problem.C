15d48d779218f8bb97883cf182074d923a125992
Ishaan <ishaan2053@gmail.com>
2023-06-08 16:03:05 +0530

    Updated dining Philosopher

--- Dining Philosopher's Problem.C
+++ Dining Philosopher's Problem.C
@@ -1,51 +1,55 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<pthread.h>
-#include<semaphore.h>
-#include<unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <semaphore.h>a
+#include <unistd.h>
 
 sem_t table;
-sem_t for[5];
+sem_t fork[5];
 
-void * philosopher(void *);
+void *philosopher(void *);
 void eat(int);
-int main()
+
+void *philosopher(void *num)
 {
-	int i,a[5];
-	pthread_t tid[5];
-	
-	sem_init(&table,0,4);
-	
-	for(i=0;i<5;i++)
-		sem_init(&for[i],0,1);
-		
-	for(i=0;i<5;i++){
-		a[i]=i;
-		pthread_create(&tid[i],NULL,philosopher,(void *)&a[i]);
-	}
-	for(i=0;i<5;i++)
-		pthread_join(tid[i],NULL);
-}
+    int phil = *(int *)num;
 
-void * philosopher(void * num)
-{
-	int phil=*(int *)num;
+    sem_wait(&table);
+    printf("\nPhilosopher %d has come to table", phil);
+    sem_wait(&fork[phil]);
+    sem_wait(&fork[(phil + 1) % 5]);
 
-	sem_wait(&table);
-	printf("\nPhilosopher %d has entered table",phil);
-	sem_wait(&for[phil]);
-	sem_wait(&for[(phil+1)%5]);
+    eat(phil);
+    sleep(2);
+    printf("\nPhilosopher %d has finished eating", phil);
 
-	eat(phil);
-	sleep(2);
-	printf("\nPhilosopher %d has finished eating",phil);
-
-	sem_post(&for[(phil+1)%5]);
-	sem_post(&for[phil]);
-	sem_post(&table);
+    sem_post(&fork[(phil + 1) % 5]);
+    sem_post(&fork[phil]);
+    sem_post(&table);
 }
 
 void eat(int phil)
 {
-	printf("\nPhilosopher %d is eating",phil);
+    printf("\nPhilosopher %d is eating", phil);
+}
+
+int main()
+{
+    int i, a[5];
+    pthread_t tid[5];
+
+    sem_init(&table, 0, 4);
+
+    for (i = 0; i < 5; i++)
+        sem_init(&fork[i], 0, 1);
+
+    for (i = 0; i < 5; i++)
+    {
+        a[i] = i;
+        pthread_create(&tid[i], NULL, philosopher, (void *)&a[i]);
+    }
+    for (i = 0; i < 5; i++)
+        pthread_join(tid[i], NULL);
+
+    return 0;
 }
